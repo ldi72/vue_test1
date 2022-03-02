43515fdea669b096ba7f49cb4bd04bfb6faa1866
ldi72 <dmy_litvin@mail.ru>
2022-03-02 18:31:09 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <textarea readonly rows="30" v-model="review"></textarea>
+    <br>
+    <label>Сообщение:</label>
+    <br>
+    <input id="submitText" v-model="submitText">
+    <!--input type="submit" value="Отпарвить-->
+    <br>
+    <button v-on:click="submitClick">Отправить</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
+/* eslint-disable */
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      review: '',
+      submitText: ''
+    }
+  },
+  methods: {
+    submitClick () {
+      document.getElementById('submitText').focus()
+            
+      if (this.submitText === '') return
+
+      var now = new Date()      
+      const formatDateTime = ('0' + now.getDate().toString()).slice(-2) + '.' +
+        ('0' + (now.getMonth() + 1).toString()).slice(-2) + '.' +
+        now.getFullYear().toString() + ' ' +
+        ('0' + now.getHours().toString()).slice(-2) + ':' +
+        ('0' + now.getMinutes().toString()).slice(-2) + ':' +
+        ('0' + now.getSeconds().toString()).slice(-2) + '\n'
+      
+      this.review += formatDateTime + this.submitText + '\n\n'
+      this.submitText = ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
 }
+  textarea, input {
+    width: 50%;
+  }
 </style>
